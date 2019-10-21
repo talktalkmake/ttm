ab3ec5ce4facf6719782e0c897e9fdf0767fa5be
Tom Hart <kitsonian@gmail.com>
2019-10-21 12:17:41 -0400

    Mistaken Identity
    Mistook one app with the same name for the other. Merging.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap-reboot.min.css";
+import "./css/index.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <article className="box">
+          <h1 class="quiet">
+            Tom
+            <br />
+            Hart
+          </h1>
+        </article>
+        <article class="box">
+          <img src="https://picsum.photos/200" alt="Thomson Hart" />
+        </article>
+        <article class="box">
+          <img src="https://picsum.photos/200" alt="Thomson Hart" />
+        </article>
+        <article class="box">
+          <img src="https://picsum.photos/200" alt="Thomson Hart" />
+        </article>
+        <article class="box">
+          <h3>Founder, Jolly Edition</h3>
+          <p>2012 onwards</p>
+          <p>
+            Responsibilites include: interface design, logistics, marketing,
+            project management, strategy, sales, web development
+          </p>
+        </article>
+        <article class="box">
+          <img src="https://picsum.photos/200" alt="Thomson Hart" />
+        </article>
+        <article class="box">
+          <img src="https://picsum.photos/200" alt="Thomson Hart" />
+        </article>
+        <article class="box">
+          <img src="https://picsum.photos/200" alt="Thomson Hart" />
+        </article>
+        <article class="box">
+          <img src="https://picsum.photos/200" alt="Thomson Hart" />
+        </article>
+        <article class="box">
+          <img src="https://picsum.photos/200" alt="Thomson Hart" />
+        </article>
+        <article class="box">
+          <img src="https://picsum.photos/200" alt="Thomson Hart" />
+        </article>
+        <article class="box">
+          <img src="https://picsum.photos/200" alt="Thomson Hart" />
+        </article>
+      </div>
+    );
+  }
 }
 
 export default App;
